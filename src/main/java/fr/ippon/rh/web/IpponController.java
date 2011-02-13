211088183f11d7aa3b75bfc039b339dd414d1932
Julien Dubois <julien@responcia.fr>
2011-02-13 17:06:23 +0100

    Added the project files

---
+++ src/main/java/fr/ippon/rh/web/IpponController.java
@@ -0,0 +1,31 @@
+package fr.ippon.rh.web;
+
+import fr.ippon.rh.service.OffreService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.inject.Inject;
+
+/**
+ * Page Web d'offre d'emploi.
+ */
+@Controller
+public class IpponController {
+
+    @Inject
+    private OffreService offreService;
+
+	/**
+	 * Affiche l'offre..
+	 */
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView ipponRecrute() {
+        ModelAndView mv = new ModelAndView("ippon");
+        mv.addObject("offre", offreService.getOffre());
+		return mv;
+	}
+	
+}
+
---
+++ src/main/webapp/WEB-INF/views/ippon.jsp
@@ -0,0 +1,15 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Offre d'emploi</title>
+</head>
+<body>
+<h1>
+	Offre d'emploi
+</h1>
+<p>
+    ${offre}
+</p>
+</body>
+</html>
