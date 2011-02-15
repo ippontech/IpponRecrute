a852e80c3b5b680b20de251f6d0ef0c561233105
Florent Ramière <florent@ramiere.com>
2011-02-15 11:31:30 +0100

    Suite à la discussion à https://github.com/ippontech/IpponRecrute/commit/973a1f6ddec4b163af2610baed728bc7669b50a1 Et pour le plaisir utilise mockito et festAssert

--- src/test/java/fr/ippon/rh/web/IpponControllerTest.java
+++ src/test/java/fr/ippon/rh/web/IpponControllerTest.java
@@ -1,14 +1,20 @@
 package fr.ippon.rh.web;
 
-import fr.ippon.rh.service.OffreService;
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.ui.ExtendedModelMap;
-
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
-import static org.easymock.EasyMock.*;
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.fest.assertions.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.ui.ExtendedModelMap;
+
+import fr.ippon.rh.service.OffreService;
 
 /**
  * Test de la classe {@link IpponController}
@@ -19,30 +25,28 @@ public class IpponControllerTest {
     private OffreService mockOffreService;
 
     @Before
-    public void setUp(){
-        //Init
-        controller = new IpponController();
-        mockOffreService = createMock(OffreService.class);
-        ReflectionTestUtils.setField(controller, "offreService", mockOffreService);
+    public void setUp() {
+        mockOffreService = mock(OffreService.class);
+        controller = new IpponController(mockOffreService);
     }
 
     @Test
-    public void testIpponRecruteGet(){
-        //Expect
+    public void testIpponRecruteGet() {
+        // Expect
         ExtendedModelMap model = new ExtendedModelMap();
         String text = "Offre";
-        expect(mockOffreService.getOffre()).andReturn(text);
+        when(mockOffreService.getOffre()).thenReturn(text);
 
         // When
-        replay(mockOffreService);
         String returnViewName = controller.ipponRecrute(model);
 
         // Verify
-        verify(mockOffreService);
-        assertNotNull(returnViewName);
-        assertEquals("ippon", returnViewName);
-        assertEquals(1, model.size());
-        assertNotNull(model.get("offre"));
-        assertEquals(text, model.get("offre"));
+        verify(mockOffreService, times(1)).getOffre();
+        assertThat(returnViewName).isNotNull();
+        assertThat(returnViewName).isEqualTo("ippon");
+        assertThat(model).isNotNull();
+        assertThat(model).hasSize(1);
+        assertThat(model.get("offre")).isNotNull();
+        assertThat(model.get("offre")).isEqualTo(text);
     }
 }
